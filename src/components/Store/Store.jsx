ce56041d37f18dbcadea48c665f2b3ea40d98fd6
georgymukha <georgymukha@yandex.ru>
2022-06-01 05:11:24 +0600

    Ready project

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 import './Store.css';
 
 function Store(props) {
+
+    const background = `background-image: url({props.img});`;
+
   return (
     <div className='item'>
-        <img src={} alt="" className="item__img" />
+        <div style={{backgroundImage: `url(${props.img})`}} className="item__img" />
+        {/* <img src={props.img} alt="" className="item__img" /> */}
+        <h3 className="item__title">{props.title}</h3>
         <h2 className="item__name">{props.name}</h2>
         <p className="item__desc">{props.desc}</p>
-        <h3 className="item__price">{props.price}</h3>
+        {/* Нашёл в интернете интересную встроенную библиотеку для работы с цифрами */}
+        <h3 className="item__price">{new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(props.price)}</h3>
 
     </div>
   )
